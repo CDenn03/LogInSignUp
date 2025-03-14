95395dbf3a145e200a185de111e08c298daaf092
CDenn <nd.dennis03@gmail.com>
2025-03-14 20:26:48 +0300

    Added Login components

--- src/components/auth/authImage.jsx
+++ src/components/auth/authImage.jsx
@@ -1,24 +1,24 @@
 
-// import React from "react";
-// import { cn } from "@/lib/utils";
-// import { Button } from "@/components/ui/button";
-// import { Card, CardContent } from "@/components/ui/card";
-// import { Input } from "@/components/ui/input";
-// import { Label } from "@/components/ui/label";
-// import Image from "next/image";
+import React from "react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Image from "next/image";
 
-// function AuthImage() {
-//   return (
-//     <div className="bg-muted relative hidden md:block ">
-//         <Image
-// 					src = "/login.svg"
-// 					alt="Login"
-// 					className="h-full w-full object-contain dark:brightness-[0.2] dark:grayscale"
-// 					width={383.2}
-// 					height={506}
-//         />
-//     </div>	
-//   )
-// }
+function AuthImage() {
+  return (
+    <div className="bg-muted relative hidden md:block ">
+        <Image
+					src = "/login.svg"
+					alt="Login"
+					className="h-full w-full object-contain dark:brightness-[0.2] dark:grayscale"
+					width={383.2}
+					height={506}
+        />
+    </div>	
+  )
+}
 
-// export default AuthImage
+export default AuthImage
