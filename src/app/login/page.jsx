a592f51e4720815143777d3401b076ce8e337e69
CDenn <nd.dennis03@gmail.com>
2025-03-14 21:16:48 +0300

    Added some transitions using Framer Motion

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,14 +1,15 @@
 "use client"
 
 import {useState, useEffect} from 'react';
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import AuthImage from "@/components/auth/authImage"
-import LoginForm from "@/components/auth/LoginForm"
-import SignUpForm from "@/components/auth/SignUpForm"
+import AuthImage from "@/components/auth/authImage";
+import LoginForm from "@/components/auth/LoginForm";
+import SignUpForm from "@/components/auth/SignUpForm";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function LoginPage({
   className,
@@ -17,21 +18,13 @@ export default function LoginPage({
 
   const [isSignUp, setIsSignUp] = useState(false);
 
-  // useEffect(()=>{
-  //   const isSignUpData = sessionStorage.getItem('isSignUp');
-  //   if(isSignUpData){
-  //     console.log(JSON.parse(isSignUpData))
-  //     setIsSignUp(JSON.parse(isSignUpData));
-  //   }
-  // },[isSignUp])
-
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         <div className={cn("flex flex-col gap-6", className)} {...props}>
           <Card className="overflow-hidden p-0">
 
-            {isSignUp? 
+            {/* {isSignUp? 
             <CardContent className="grid p-0 md:grid-cols-2 transition-transform">
               <SignUpForm setIsSignUp={setIsSignUp}  /> 
               <AuthImage />
@@ -40,7 +33,36 @@ export default function LoginPage({
              <CardContent className="grid p-0 md:grid-cols-2 transition-transform">
               <AuthImage />
                <LoginForm setIsSignUp={setIsSignUp}  /> 
-            </CardContent>  }  
+            </CardContent>  }  */}
+            <AnimatePresence mode="wait">
+              {isSignUp ? (
+                <motion.div
+                  key="signup"
+                  initial={{ x: "100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "-100%", opacity: 0 }}
+                  transition={{ type: "bounce", bounce: 0.25 }}
+                  className="grid p-0 md:grid-cols-2"
+                >
+                  <SignUpForm setIsSignUp={setIsSignUp} />
+                  <AuthImage />
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="login"
+                  initial={{ x: "-100%", opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  exit={{ x: "100%", opacity: 0 }}
+                  transition={{ type: "bounce", bounce: 0.25 }}
+                  className="grid p-0 md:grid-cols-2"
+                >
+                  <AuthImage />
+                  <LoginForm setIsSignUp={setIsSignUp} />
+                </motion.div>
+              )}
+            </AnimatePresence>
+            
+             
           </Card>    
         </div>
       </div>
--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -11,7 +11,7 @@ function SignUpForm({setIsSignUp}) {
 		setIsSignUp(false);
 	}
   return (
-    <form className="p-6 md:p-8 bg-transparent">
+    <form className="p-6 md:p-8">
 			<div className="flex flex-col gap-6">
 					<div
 						className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
